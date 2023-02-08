7f610f7760b42ac7f69ca6d31009d8436eeed173
SamkharadzeMerab <samkharadzemerab@gmail.com>
2023-02-09 00:44:09 +0400

    fix a bug
    select input

--- js/education.js
+++ js/education.js
@@ -1,7 +1,8 @@
-const down = document.querySelector(".down_img");
+const down = document.querySelector(".select_input_vl");
 const options = document.querySelector(".options");
 const option = document.querySelectorAll(".option");
 const start_date_input = document.querySelector(".start_date_input");
+const anywhere = document.querySelector("body");
 
 option.forEach((oneOption) => {
   oneOption.addEventListener("click", choose);
